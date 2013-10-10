21c7834b883a99892c046262e728f14967e7a899
Kevin Shopoff <kshopoff@gmail.com>
2013-10-09 21:53:53 -0500

    This is the first draft of the Hot or Cold game

---
+++ hotOrCold.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var target = Math.floor(Math.random()*100);
+	var correct = false;
+	alert("Here we go!" + target);
+	while (!correct) {
+		var guess = prompt("Enter your guess between 1 and 100");
+		if (guess == target) {
+			correct = true;
+			alert("You guessed " + target + " correctly!");
+		} else if (guess < target){
+			alert("Your guess of " + guess + " is too low.  Please try again.");
+		} else {
+			alert("Your guess of " + guess + " is too high.  Please try again.")
+		};
+	};
+	
+});
